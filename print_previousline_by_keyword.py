298e55e74bb57c49a07bbae78519f1c1c442d26f
Isaac Liao <isaac.liao@ubitus.net>
2015-10-22 12:08:57 +0800

    support print previous line, search by key

---
+++ print_previousline_by_keyword.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import glob
+import re
+import itertools
+
+if len(sys.argv) != 3:
+    sys.exit("please specify filepath and search keyword. Ex. python listpreviousline.py #{filepath} #{searchKeyword1}")
+    
+filepath = sys.argv[1];
+keyword = sys.argv[2]
+print "search file:", filepath
+print 'keyword:', keyword;     
+ 
+for subdir, dirs, filenames in os.walk(filepath):
+    for filename in filenames:
+        absoluteFilePath = os.path.join(subdir, filename)
+        print('process:' + absoluteFilePath)
+
+        previousLine = ''
+        with open(absoluteFilePath) as file:
+            for line in file.readlines():
+                if keyword in line:
+                    print previousLine
+                    print line
+                    print '============================================'
+                previousLine = line
+            
+                
